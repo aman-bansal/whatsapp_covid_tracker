9cfe6334447f5b43f555cafc510d57c348d10b21
aman-bansal <bansalaman2905@gmail.com>
2020-07-16 15:40:49 +0530

    init

--- pkg/main.go
+++ pkg/main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	var value []byte
-	err := json.NewDecoder(r.Body).Decode(&value)
-	if err != nil {
-		log.Println("error", err)
-	}
-	log.Println(value)
-	fmt.Fprintf(w, "Hi there, I love %s %s!", r.URL.Path[1:], value)
+	log.Println(r.Form)
+	fmt.Fprintf(w, "Hi there, I love %s %s!", r.URL.Path[1:])
 }
 
 
